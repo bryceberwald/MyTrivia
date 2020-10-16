ad728d0115e3a377604e5017b94a281f61d57008
Bryce Berwald <bryce@berwald.io>
2020-10-16 16:01:33 -0700

    Initial commit

---
+++ app/src/main/java/edu/bberwald0/mytrivia/MainActivity.java
@@ -0,0 +1,88 @@
+package edu.bberwald0.mytrivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private int mCurrentIndex = 0;
+
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_text, true),
+            new Question(R.string.question_text_two, true),
+            new Question(R.string.question_text_three, false),
+            new Question(R.string.question_text_four, false),
+            new Question(R.string.question_text_five, true)
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //does nothing yet, but it will
+                checkAnswer(true);
+            }
+        });
+
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //does nothing yet, but it will
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //does nothing yet, but it will
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+    }
+
+    private  void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+    }
+}
